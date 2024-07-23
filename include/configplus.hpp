b3f8462ea9e4b78d7bb81ae70c3efdfade74e854
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-07-23 18:21:45 +0300

    not working1

--- include/configplus.hpp
+++ include/configplus.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <string>
 #include <vector>
 #include <type_traits>
+#include <cstddef>
+#include <iostream>
 
 class IConfigClass
 {
@@ -25,15 +26,46 @@ using is_config_class = std::is_base_of<IConfigClass, T>;
 template <typename T>
 struct is_vector : std::false_type {};
 
+// Helper to get the class type from a member pointer
 template <typename T>
-struct VariableType
+struct get_member_class;
+
+// Specialization for member pointer types
+template <typename ClassType, typename MemberType>
+struct get_member_class<MemberType ClassType::*> {
+    using type = ClassType;
+};
+
+template <typename T>
+struct get_class_from_member;
+
+template <typename ClassT, typename MemberT>
+struct get_class_from_member<MemberT ClassT::*>
 {
-    static_assert(is_simple_type<T>::value || is_config_class<T>::value || is_vector<T>::value, "Invalid type");
+    /* class Test1
+     * {
+     *    int a;
+     *    float b;
+    */
+    // we have the a or b members, 
+    // we must return the class type Test1 
+    
+    using type = ClassT;
+};
+
+template <typename T>
+struct VariableType 
+{
+    // static_assert(is_class_inherits_from_IConfigClass_v<T>, "Invalid type");
+    // static_assert(is_simple_type<T>::value || is_config_class<T>::value || is_vector<T>::value, "Invalid type");
 };
 
 template <typename T>
 struct Variable : public VariableType<T>
 {
+
+    using ClassType = typename get_class_from_member<T>::type;
+
     T value;
     operator T() const { return value; }
     Variable& operator=(const T& v) { value = v; return *this; }
