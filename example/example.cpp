4a747c6277542792ba7502a3e7c0d464b769904b
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-07-23 17:09:54 +0300

    tests going on

--- example/example.cpp
+++ example/example.cpp
@@ -4,13 +4,14 @@
 
 int main()
 {
-    Config test;
-    test.myInt.value = 42;
-    test.myFloat.value = 3.14f;
-    test.otherClass.value.myInt.value = 24;
+    Config config;
+    config.var1 = 1;
+    config.var2 = 2.5f;
+    config.var3.value.var1 = 3;
 
-    std::cout << "myInt: " << test.myInt<< std::endl;
-    std::cout << "myFloat: " << test.myFloat<< std::endl;
+    std::cout << config.var1.value << std::endl;
+    std::cout << config.var2.value << std::endl;
+    std::cout << config.var3.value.var1.value << std::endl;
 
     return 0;
 }
--- include/configplus.hpp
+++ include/configplus.hpp
@@ -10,40 +10,45 @@ protected:
     std::vector<size_t> m_VariableOffsets;
 };
 
-// make sure a typename is derived from IConfigClass or
-// can be converted to string
+// there are 3 options that T can be
+//   1 - a simple type (int, float, double, etc) 
+//   2 - a class that inherits from IConfigClass (another config class)
+//   3 - a vector which can be nested as many times as needed which 
+//         will eventually end up being one of the above 2 options
+
 template <typename T>
-using IsDerivedOrConvertibleToString = std::disjunction<std::is_base_of<IConfigClass, T>, std::is_convertible<T, std::string>>;
+using is_simple_type = std::disjunction<std::is_arithmetic<T>, std::is_enum<T>>;
 
-// check template type inherits from IConfigClass or 
-// is a type which can be converted to string (like
-// int, float, double, etc.) also make sure the Variable
-// is in a class that inherits from IConfigClass
-template <typename T, typename = std::enable_if<std::is_convertible<T, std::string>::value>>
-struct ConfigVariable
+template <typename T>   
+using is_config_class = std::is_base_of<IConfigClass, T>;
+
+template <typename T>
+struct is_vector : std::false_type {};
+
+template <typename T>
+struct VariableType
 {
-    std::string name;
+    static_assert(is_simple_type<T>::value || is_config_class<T>::value || is_vector<T>::value, "Invalid type");
+};
+
+template <typename T>
+struct Variable : public VariableType<T>
+{
     T value;
-    ConfigVariable(std::string name, T value = T{}) : name(name), value(value) {}
-
     operator T() const { return value; }
-    const T& operator=(const T& rhs) { return value = rhs; }
+    Variable& operator=(const T& v) { value = v; return *this; }
 };
 
-class TheOtherClass : public IConfigClass
+
+struct Config1 : public IConfigClass
 {
-public:
-    ConfigVariable<int> myInt{ "TheInt2" };
-    ConfigVariable<float> myFloat{ "TheFloat2" };
+    Variable<int> var1;
+    Variable<float> var2;
 };
 
-// Example usage
-class Config : public IConfigClass
+struct Config : public IConfigClass
 {
-public:
-    ConfigVariable<int> myInt{ "TheInt" };
-    ConfigVariable<float> myFloat{ "TheFloat" };
-
-    ConfigVariable<TheOtherClass> otherClass{ "TheOtherClass" };
+    Variable<int> var1;
+    Variable<float> var2;
+    Variable<Config1> var3;
 };
-
